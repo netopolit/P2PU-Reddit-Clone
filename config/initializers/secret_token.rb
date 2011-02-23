4eb1445987c18e41044ff5fdfd4954a0c8e906a0
Mislav Paparella <mpaparel@me.com>
2011-02-23 16:51:08 +0100

    Initial commit. Replaced prototype with jQuery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = 'fb9931212f275a9f55aea765c6193910977de7cecd9eccaf5ecbe5d82ebdbde0cc62befb44e7705e14fbbc999a5f406dc40f83ca904127cb91745f52cb31e316'
