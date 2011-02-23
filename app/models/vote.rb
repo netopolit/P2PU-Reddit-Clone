2023d09ab71ef55adfc489ac5b7f13eaab940f81
Mislav Paparella <mpaparel@me.com>
2011-02-23 22:37:15 +0100

    vote validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -15,6 +15,9 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
 
+  validates :user_id, :presence => true
+  validates :link_id, :presence => true
+  #Should ensure max one vote by specific user per article?
   validates_uniqueness_of :link_id, :scope => :user_id
   
 end
