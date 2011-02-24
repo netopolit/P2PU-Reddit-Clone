b5ef38adb3d713cfabcc6591edbd13028b6de8f6
Mislav Paparella <mpaparel@me.com>
2011-02-24 21:31:38 +0100

    arrows now show if link was woted up or down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :voted?
+  helper_method :voted?, :voted_up?
 
   def voted?(link)
     !current_user.votes.where(:link_id => link.id).empty?
   end
 
   def voted_up?(link)
-    current_user.votes.where(:link_id => link.id) == 1
+    vote = current_user.votes.where(:link_id => link.id).first
+    vote.points == 1
   end
   
 end
